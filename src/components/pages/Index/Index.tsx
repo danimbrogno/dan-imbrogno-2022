5ed5763b39da184570e9d036be92eb1284bb9309
Dan Imbrogno <dan@3vl.ca>
2023-02-05 21:53:49 -0500

    Implements parallax

--- src/components/pages/Index/Index.tsx
+++ src/components/pages/Index/Index.tsx
@@ -2,23 +2,26 @@ import * as React from "react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../../../theme";
 import { Main } from "../../shared/Main";
-import { Header } from "./components/Header";
 import { Work } from "./components/Work/Work";
 import { Education } from "./components/Education/Education";
 import { Technologies } from "./components/Technologies/Technologies";
-import { AboutMe } from "./components/AboutMe";
+import { AboutMe } from "./components/AboutMe/AboutMe";
 import { Menu } from "./components/Menu";
+import { ParallaxProvider } from "react-scroll-parallax";
+import { Header } from "./components/Header";
 
 export const Index: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <Main>
-        <Menu />
-        <Header />
-        <AboutMe />
-        <Work />
-        <Education />
-        <Technologies />
+        <ParallaxProvider>
+          <Menu />
+          <Header />
+          <AboutMe />
+          <Work />
+          <Education />
+          <Technologies />
+        </ParallaxProvider>
       </Main>
     </ThemeProvider>
   );
--- src/components/pages/Index/components/Header.tsx
+++ src/components/pages/Index/components/Header.tsx
@@ -1,38 +1,40 @@
 import React from "react";
-import style from "styled-components";
+import { Parallax } from "react-scroll-parallax";
 import Github from "../../../../assets/github-icon.svg";
 import Email from "../../../../assets/email-icon.svg";
-import Cabin from "../../../../images/cabin.svg";
+import GradientImg from "../../../../images/header-layer1.svg";
+import Stars1Img from "../../../../images/header-layer2.svg";
+import Stars2Img from "../../../../images/header-layer3.svg";
+import MoonImg from "../../../../images/header-layer4.svg";
+import SceneryImg from "../../../../images/header-layer5.svg";
+import CabinImg from "../../../../images/header-layer6.svg";
+import style from "styled-components";
 import { graphql, useStaticQuery } from "gatsby";
 
 const Banner = style.div`
-height: 100vh;
-width: 100vw;
-min-height:480px;
-background: #000 url(${Cabin}) no-repeat 100% 0%;
-background-size: cover;
-display: flex;
-align-items: flex-start;
-justify-content: center;
-font-size: 2em;
-flex-direction: column;
-color: ${(props) => props.theme.text.primaryColor}};
-@media (max-width: ${(props) => props.theme.breakPoints.tablet}px) {
-  align-items: center;
-  background: #000 url(${Cabin}) no-repeat 90% 0%;
-  background-size: cover;
-}
+  pointer-events: none;
+  min-height:480px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  font-size: 2em;
+  flex-direction: column;
+  color: ${(props) => props.theme.text.primaryColor}};
+  @media (max-width: ${(props) => props.theme.breakPoints.tablet}px) {
+    align-items: center;
+  }
 `;
 
 const Content = style.div`
-@media (max-width: ${(props) => props.theme.breakPoints.tablet}px) {
-  margin: 0 auto;
-  width: 50%;
-}
-@media (max-width: ${(props) => props.theme.breakPoints.laptop}px) {
-  margin: 0 auto;
-  width: 90%;
-  top:0;
+
+  @media (max-width: ${(props) => props.theme.breakPoints.tablet}px) {
+    margin: 0 auto;
+    width: 50%;
+  }
+  @media (max-width: ${(props) => props.theme.breakPoints.laptop}px) {
+    margin: 0 auto;
+    width: 90%;
+    top:0;
   }
   width: 40%;
   margin-left:80px;
@@ -79,9 +81,53 @@ const Button = style.a`
   justify-content: center;
 `;
 
-export const Header: React.FC = () => {
-  const data = useStaticQuery<Queries.HeaderQuery>(graphql`
-    query Header {
+const MoonDiv = style.div`
+    position: absolute;
+    background-image: url(${MoonImg});
+    background-position: top right -15%;
+    background-size: 160px 160px;
+    background-repeat: no-repeat;
+    height: 100vh;
+    width: 100vw;
+    @media (min-width: ${(props) => props.theme.breakPoints.mobileL}px) {
+      background-position: top right 0;
+      background-size: 200px 200px;
+    }
+    @media (min-width: ${(props) => props.theme.breakPoints.tablet}px) {
+    background-size: 240px 240px;
+  }
+`;
+
+const SceneryDiv = style.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  background-image: url(${SceneryImg});
+  background-position: bottom center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const CabinDiv = style.div`
+  position: absolute;
+  top: 20px;
+  left: 0;
+  background-image: url(${CabinImg});
+  background-position: bottom right 25%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+  @media (min-width: ${(props) => props.theme.breakPoints.mobileL}px) {
+    background-position: bottom right 0%;
+  }
+`;
+
+export const Header = () => {
+  const data = useStaticQuery<Queries.HeaderDataQuery>(graphql`
+    query HeaderData {
       site {
         siteMetadata {
           title
@@ -102,19 +148,89 @@ export const Header: React.FC = () => {
     : "";
 
   return (
-    <Banner>
-      <Content>
-        <Name>{data.site?.siteMetadata?.title}</Name>
-        <Description>{data.site?.siteMetadata?.description}</Description>
-        <Buttons>
-          <Button href={mailToLink}>
-            <Email height="64" width="64" fill="white" />
-          </Button>
-          <Button href={githubLink}>
-            <Github height="44" width="44" fill="white" />
-          </Button>
-        </Buttons>
-      </Content>
-    </Banner>
+    <div
+      style={{
+        position: "relative",
+        backgroundImage: `url(${GradientImg})`,
+        backgroundSize: "cover",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <Parallax
+        speed={-10}
+        style={{ height: "100vh", position: "absolute", top: 0 }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            backgroundImage: `url(${Stars1Img})`,
+            backgroundSize: "cover",
+            width: "100vw",
+            height: "100vh",
+          }}
+        />
+      </Parallax>
+      <Parallax
+        speed={-8}
+        style={{ height: "100vh", position: "absolute", top: 0 }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            backgroundImage: `url(${Stars2Img})`,
+            backgroundSize: "cover",
+            width: "100vw",
+            height: "100vh",
+          }}
+        />
+      </Parallax>
+      <Parallax
+        speed={-6}
+        style={{
+          height: "100vh",
+          width: "100vw",
+          position: "absolute",
+          top: 0,
+        }}
+      >
+        <MoonDiv />
+      </Parallax>
+      <Parallax
+        speed={-4}
+        style={{ height: "100vh", position: "absolute", top: 0 }}
+      >
+        <SceneryDiv />
+      </Parallax>
+      <Parallax
+        speed={0}
+        style={{ height: "100vh", position: "absolute", top: 0 }}
+      >
+        <CabinDiv />
+      </Parallax>
+      <Parallax
+        speed={-12}
+        style={{ height: "100vh", position: "absolute", top: 0 }}
+      >
+        <Banner>
+          <Content>
+            <Name>{data.site?.siteMetadata?.title}</Name>
+            <Description>{data.site?.siteMetadata?.description}</Description>
+            <Buttons>
+              <Button href={mailToLink}>
+                <Email height="64" width="64" fill="white" />
+              </Button>
+              <Button href={githubLink}>
+                <Github height="44" width="44" fill="white" />
+              </Button>
+            </Buttons>
+          </Content>
+        </Banner>
+      </Parallax>
+    </div>
   );
 };
